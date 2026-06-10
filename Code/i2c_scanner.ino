0d886249cc94825fc78d897b1f6ec26e7a8746c0
ULLAS DEVANG M <ullasdevang44@gmail.com>
2026-06-10 09:26:30 +0530

    Project Code

--- Code/i2c_scanner.ino
+++ Code/i2c_scanner.ino
@@ -1,1 +1,33 @@
+#include <Wire.h>
 
+void setup()
+{
+  Wire.begin();
+  Serial.begin(9600);
+
+  Serial.println("I2C Scanner");
+}
+
+void loop()
+{
+  byte error, address;
+  int count = 0;
+
+  for(address = 1; address < 127; address++)
+  {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if(error == 0)
+    {
+      Serial.print("Device found at 0x");
+      Serial.println(address, HEX);
+      count++;
+    }
+  }
+
+  if(count == 0)
+    Serial.println("No devices found");
+
+  delay(3000);
+}
